b192a44627d4d30595cd8e0042ff4d7487e65b6b
Lee <leedeboom@gmail.com>
2017-06-20 22:21:00 -0500

    converted devise views to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get '/position/:position_id/reasons', to: 'reasons#create'
 
   root 'topics#index'
 
   resources :topics, only: [:index, :create, :new, :show]
   resources :user, only: [:create, :new]
-  
+
   resources :position, only: [] do
     get :almost
     post :select
