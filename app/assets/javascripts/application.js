dc8e057269106409abee4f184557f6ed1c14a6d5
Lee <leedeboom@gmail.com>
2017-07-11 00:48:48 -0500

    random reasons showing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,15 +50,44 @@ $( document ).ready(function() {
 
   $('#topics > .topic-container > .topic > .topic-side.topic-side-left').tooltipster({
     theme: 'tooltipster-noir',
-    side: 'left',
-    theme: ['tooltipster-light'],
+    side: ['left', 'right'],
+    theme: 'tooltipster-light',
     animation: 'fall'
   });
 
   $('#topics > .topic-container > .topic > .topic-side.topic-side-right').tooltipster({
     theme: 'tooltipster-noir',
-    side: 'right',
-    theme: ['tooltipster-light'],
+    side: ['right', 'left'],
+    theme: 'tooltipster-light',
     animation: 'fall'
   });
+
+  window.setInterval(function(){
+    var positions = $( "#topics > .topic-container > .topic > .topic-side" )
+    var position = $(positions[Math.floor(Math.random()*positions.length)])
+    if (!isElementInViewport(position)){
+      while(!isElementInViewport(position)){
+        var position = $(positions[Math.floor(Math.random()*positions.length)])
+      }
+    }
+    $(position).tooltipster('open');
+    setTimeout(function (){
+      $(position).tooltipster('close');
+    }, 10000);
+  }, 20000);
+
+  function isElementInViewport (el) {
+      if (typeof jQuery === "function" && el instanceof jQuery) {
+          el = el[0];
+      }
+
+      var rect = el.getBoundingClientRect();
+
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+      );
+  }
 });
