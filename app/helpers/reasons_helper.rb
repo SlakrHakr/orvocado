36497bb6c3d7ac77ac9e54da616085163119f396
Lee <leedeboom@gmail.com>
2017-08-09 23:36:53 -0500

    renders links properly

--- app/helpers/reasons_helper.rb
+++ app/helpers/reasons_helper.rb
@@ -1,2 +1,17 @@
 module ReasonsHelper
+  def link_urls input
+      output = ''
+      input.split('<br/>').each do |line|
+        line.split(' ').each do |word|
+          escaped = word
+          escaped = escaped.gsub(/( |^)(http|https):\/\/([^\s]*\.[^\s]*)( |$)/){ "<a href='#{$2}://#{$3}'>#{$2}://#{$3}</a>" }
+          escaped = escaped.gsub(/( |^)\[(.*)\|(http|https):\/\/([^\s]*\.[^\s]*)\]( |$)/){ "<a href='#{$3}://#{$4}'>#{$2}</a>" }
+
+          output += escaped
+          output += ' '
+        end
+        output += '<br/>'
+      end
+      output
+  end
 end
