ebb66337b02882d8ab4070ae81eec6996cfe119d
Lee DeBoom <Lee.DeBoom@Cerner.com>
2018-07-31 13:40:59 -0500

    one more shot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,16 +23,6 @@
 //= require user
 //= require reasons
 
-// Hotjar Tracking Code for www.orvocado.com
-(function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:965847,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-
 $( document ).ready(function() {
   $( document ).ajaxError(function( event, jqxhr, settings, thrownError ) {
     if (jqxhr.status == 401){
